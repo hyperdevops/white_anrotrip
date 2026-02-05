0a34f2a1ca1000cecd24c4d0286bf3bcba0fd4be
hyperdevops <danfunk84@yandex.ru>
2026-02-05 23:02:00 +0500

    modal footer

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,7 +1,14 @@
-// Global window extension declarations for gift modal functionality
-
 interface Window {
   openGiftModal: () => void;
   closeGiftModal: () => void;
+  openCallbackModal: () => void;
+  closeCallbackModal: (event?: Event) => void;
   initScrollReveal: () => void;
+  lockScroll: () => void;
+  unlockScroll: () => void;
+  openLightbox: (index: number) => void;
+  closeLightbox: () => void;
+  changeSlide: (direction: number) => void;
+  toggleZoom: (event: Event) => void;
+  closeReviewForm: () => void;
 }
